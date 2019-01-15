3379c3b0fb21144cd193baac8c12cf5be8d2f695
Christian Bedwell <Christian.8edwell@gmail.com>
2019-01-15 18:20:59 -0500

    Added event listener for 'Settings' button in navigation activity

--- app/src/main/java/com/example/gtmandroidapplication/NavigationActivity.java
+++ app/src/main/java/com/example/gtmandroidapplication/NavigationActivity.java
@@ -102,6 +102,10 @@ public class NavigationActivity extends AppCompatActivity
             fragmentManager.beginTransaction()
                     .replace(R.id.content_frame, new GalleryFragment())
                     .commit();
+        } else if (id == R.id.nav_settings_layout) {
+            fragmentManager.beginTransaction()
+                    .replace(R.id.content_frame, new SettingsFragment())
+                    .commit();
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
