a2a96dd3fd25014cd8f06d61be30ccfc07fa1421
HBedwell24 <bedwellhb@gmail.com>
2019-01-16 12:39:58 -0500

    Fixed an issue where the onClick method for registerLink was not working as presupposed.

--- app/src/main/java/com/example/gtmandroidapplication/LoginActivity.java
+++ app/src/main/java/com/example/gtmandroidapplication/LoginActivity.java
@@ -9,32 +9,33 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import com.android.volley.Response;
-
 import org.json.JSONException;
 import org.json.JSONObject;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button bLogin;
+    TextView registerLink;
+    EditText etEmail, etPassword;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        final EditText etEmail = (EditText) findViewById(R.id.etEmail);
-        final EditText etPassword = (EditText) findViewById(R.id.etPassword);
-        final Button bLogin = (Button) findViewById(R.id.bLogin);
-        final TextView registerLink = (TextView) findViewById(R.id.registerLink);
+        etEmail = (EditText) findViewById(R.id.etEmail);
+        etPassword = (EditText) findViewById(R.id.etPassword);
+        bLogin = (Button) findViewById(R.id.bLogin);
+        registerLink = (TextView) findViewById(R.id.registerLink);
 
-        registerLink.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent registerIntent = new Intent(LoginActivity.this, RegisterActivity.class);
-                LoginActivity.this.startActivity(registerIntent);
-            }
-        });
+        bLogin.setOnClickListener(this);
+        registerLink.setOnClickListener(this);
+    }
 
-        bLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.bLogin:
                 final String email = etEmail.getText().toString();
                 final String password = etPassword.getText().toString();
 
@@ -46,24 +47,28 @@ public class LoginActivity extends AppCompatActivity {
                             JSONObject jsonResponse = new JSONObject(response);
                             boolean success = jsonResponse.getBoolean("success");
 
-                            if(success) {
+                            if (success) {
                                 Intent intent = new Intent(LoginActivity.this, HomeFragment.class);
                                 LoginActivity.this.startActivity(intent);
-                            }
-                            else {
+                            } else {
                                 AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
                                 builder.setMessage("Login Failed").setNegativeButton("Retry", null).create().show();
                             }
-                        }
-                        catch(JSONException e) {
+                        } catch (JSONException e) {
                             e.printStackTrace();
                         }
                     }
                 };
-               //LoginRequest loginRequest = new LoginRequest(email, password, responseListener);
-               //RequestQueue queue = Volley.newRequestQueue(LoginActivity.this);
-               //queue.add(loginRequest);
-            }
-        });
+                break;
+
+            case R.id.registerLink:
+                Intent registerIntent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(registerIntent);
+                break;
+        }
     }
+    //LoginRequest loginRequest = new LoginRequest(email, password, responseListener);
+    //RequestQueue queue = Volley.newRequestQueue(LoginActivity.this);
+    //queue.add(loginRequest);
 }
+
