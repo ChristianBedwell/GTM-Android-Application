fb2265509ab94b59acb911be903e3ebd33d215b1
Christian Bedwell <Christian.8edwell@gmail.com>
2019-01-21 11:55:59 -0500

    Set action bar title to menu item selected from navigation

--- app/src/main/java/com/example/gtmandroidapplication/JobsFragment.java
+++ app/src/main/java/com/example/gtmandroidapplication/JobsFragment.java
@@ -1,21 +1,75 @@
 package com.example.gtmandroidapplication;
 
+import android.app.DatePickerDialog;
+import android.graphics.Bitmap;
+import android.graphics.Color;
+
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import java.util.Calendar;
+
+import com.kd.dynamic.calendar.generator.ImageGenerator;
 
 public class JobsFragment extends Fragment {
 
     View myView;
+    EditText mDateEditText;
+    Calendar mCurrentDate;
+    Bitmap mGeneratedDateIcon;
+    ImageGenerator mImageGenerator;
+    ImageView mDisplayGeneratedImage;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         myView = inflater.inflate(R.layout.jobs_layout, container,false);
+        mImageGenerator = new ImageGenerator(this.getActivity());
+        mDateEditText = (EditText) myView.findViewById(R.id.txtDateEntered);
+        mDisplayGeneratedImage = (ImageView) myView.findViewById(R.id.imageGen);
+
+        mImageGenerator.setIconSize(50,50);
+        mImageGenerator.setDateSize(30);
+        mImageGenerator.setMonthSize(10);
+
+        mImageGenerator.setDatePosition(42);
+        mImageGenerator.setMonthPosition(14);
+
+        mImageGenerator.setDateColor(Color.parseColor("#3c6eaf"));
+        mImageGenerator.setMonthColor(Color.WHITE);
+
+        mImageGenerator.setStorageToSDCard(true);
+
+        mDateEditText.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                mCurrentDate = Calendar.getInstance();
+                int year = mCurrentDate.get(Calendar.YEAR);
+                int month = mCurrentDate.get(Calendar.MONTH);
+                int day = mCurrentDate.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog mDatePicker = new DatePickerDialog((FragmentActivity)view.getContext(), new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker datePicker, int selectedYear, int selectedMonth, int selectedDay) {
+                        mDateEditText.setText(selectedDay+"-"+selectedMonth+"-"+selectedYear);
+
+                        mCurrentDate.set(selectedYear, selectedMonth, selectedDay);
+                        mGeneratedDateIcon = mImageGenerator.generateDateImage(mCurrentDate, R.drawable.empty_calendar);
+                        mDisplayGeneratedImage.setImageBitmap(mGeneratedDateIcon);
+                    }
+                }, year, month, day);
+                mDatePicker.show();
+            }
+        });
         return myView;
     }
 }
