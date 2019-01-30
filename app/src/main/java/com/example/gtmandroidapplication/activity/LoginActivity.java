8fcca77f43b3773d95e7d74408d002077afe0251
Christian Bedwell <Christian.8edwell@gmail.com>
2019-01-30 17:04:13 -0500

    Organized class files for activity, app, fragment, and helper

--- app/src/main/java/com/example/gtmandroidapplication/activity/LoginActivity.java
+++ app/src/main/java/com/example/gtmandroidapplication/activity/LoginActivity.java
@@ -1,15 +1,16 @@
 package com.example.gtmandroidapplication.activity;
 
-import android.app.AlertDialog;
+import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.volley.Request.Method;
 import com.android.volley.Response;
@@ -18,25 +19,28 @@ import com.android.volley.toolbox.StringRequest;
 
 import org.json.JSONException;
 import org.json.JSONObject;
-import com.android.volley.toolbox.Volley;
-import com.example.gtmandroidapplication.fragment.HomeFragment;
-import com.example.gtmandroidapplication.LoginRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import com.example.gtmandroidapplication.R;
+import com.example.gtmandroidapplication.app.AppConfig;
+import com.example.gtmandroidapplication.app.AppController;
 import com.example.gtmandroidapplication.helper.SQLiteHandler;
 import com.example.gtmandroidapplication.helper.SessionManager;
+import com.example.gtmandroidapplication.fragment.HomeFragment;
 
-public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+public abstract class LoginActivity extends Activity implements View.OnClickListener {
 
     private static final String TAG = RegisterActivity.class.getSimpleName();
     private Button bLogin;
-    private Button registerLink;
+    private Button bRegister;
     private EditText etEmail, etPassword;
     private ProgressDialog pDialog;
     private SessionManager session;
     private SQLiteHandler db;
     private TextView tvForgotPassword;
     private CheckBox cbRememberMe;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,62 +52,150 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         bLogin = (Button) findViewById(R.id.bLogin);
         tvForgotPassword = (TextView) findViewById(R.id.tvForgotPassword);
         cbRememberMe = (CheckBox) findViewById(R.id.cbRememberMe);
-        registerLink = (Button) findViewById(R.id.registerLink);
+        bRegister = (Button) findViewById(R.id.bRegister);
 
         bLogin.setOnClickListener(this);
         tvForgotPassword.setOnClickListener(this);
         cbRememberMe.setOnClickListener(this);
-        registerLink.setOnClickListener(this);
+        bRegister.setOnClickListener(this);
+
+        pDialog = new ProgressDialog(this);
+        pDialog.setCancelable(false);
+
+        db = new SQLiteHandler(getApplicationContext());
+
+        session = new SessionManager(getApplicationContext());
+
+        // Check if user is already logged in or not
+        if (session.isLoggedIn()) {
+            // User is already logged in. Take him to main activity
+            Intent intent = new Intent(LoginActivity.this, HomeFragment.class);
+            startActivity(intent);
+            finish();
+        }
+
+        // Login button Click Event
+        bLogin.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                String email = etEmail.getText().toString().trim();
+                String password = etPassword.getText().toString().trim();
+
+                // Check for empty data in the form
+                if (!email.isEmpty() && !password.isEmpty()) {
+                    // login user
+                    checkLogin(email, password);
+                } else {
+                    // Prompt user to enter credentials
+                    Toast.makeText(getApplicationContext(),
+                            "Please enter the credentials!", Toast.LENGTH_LONG)
+                            .show();
+                }
+            }
+        });
+
+        // Link to Register Screen
+        bRegister.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(),
+                        RegisterActivity.class);
+                startActivity(i);
+                finish();
+            }
+        });
     }
 
-    @Override
-    public void onClick(View view) {
-        switch (view.getId()) {
-            case R.id.bLogin:
-                final String email = etEmail.getText().toString();
-                final String password = etPassword.getText().toString();
+    /**
+     * function to verify login details in mysql db
+     * */
+    private void checkLogin(final String email, final String password) {
+        // Tag used to cancel the request
+        String tag_string_req = "req_login";
 
-                Response.Listener<String> responseListener = new Response.Listener<String>() {
+        pDialog.setMessage("Logging in ...");
+        showDialog();
 
-                    @Override
-                    public void onResponse(String response) {
-                        try {
-                            JSONObject jsonResponse = new JSONObject(response);
-                            boolean success = jsonResponse.getBoolean("success");
+        StringRequest strReq = new StringRequest(Method.POST,
+                AppConfig.URL_LOGIN, new Response.Listener<String>() {
 
-                            if (success) {
-                                Intent intent = new Intent(LoginActivity.this, HomeFragment.class);
-                                LoginActivity.this.startActivity(intent);
-                            }
-                            else {
-                                AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
-                                builder.setMessage("Login Failed").setNegativeButton("Retry", null).create().show();
-                            }
-                        }
-                        catch (JSONException e) {
-                            e.printStackTrace();
-                        }
+            @Override
+            public void onResponse(String response) {
+                Log.d(TAG, "Login Response: " + response.toString());
+                hideDialog();
+
+                try {
+                    JSONObject jObj = new JSONObject(response);
+                    boolean error = jObj.getBoolean("error");
+
+                    // Check for error node in json
+                    if (!error) {
+                        // user successfully logged in
+                        // Create login session
+                        session.setLogin(true);
+
+                        // Now store the user in SQLite
+                        String uid = jObj.getString("uid");
+
+                        JSONObject user = jObj.getJSONObject("user");
+                        String name = user.getString("name");
+                        String email = user.getString("email");
+                        String created_at = user
+                                .getString("created_at");
+
+                        // Inserting row in users table
+                        db.addUser(name, email, uid, created_at);
+
+                        // Launch main activity
+                        Intent intent = new Intent(LoginActivity.this,
+                                HomeFragment.class);
+                        startActivity(intent);
+                        finish();
+                    } else {
+                        // Error in login. Get the error message
+                        String errorMsg = jObj.getString("error_msg");
+                        Toast.makeText(getApplicationContext(),
+                                errorMsg, Toast.LENGTH_LONG).show();
                     }
-                };
-                LoginRequest loginRequest = new LoginRequest(email, password, responseListener);
-                RequestQueue queue = Volley.newRequestQueue(LoginActivity.this);
-                queue.add(loginRequest);
-                break;
+                }
+                catch (JSONException e) {
+                    // JSON error
+                    e.printStackTrace();
+                    Toast.makeText(getApplicationContext(), "Json error: " + e.getMessage(), Toast.LENGTH_LONG).show();
+                }
+            }
+        }, new Response.ErrorListener() {
 
-            case R.id.registerLink:
-                Intent registerIntent = new Intent(LoginActivity.this, RegisterActivity.class);
-                startActivity(registerIntent);
-                break;
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e(TAG, "Login Error: " + error.getMessage());
+                Toast.makeText(getApplicationContext(),
+                        error.getMessage(), Toast.LENGTH_LONG).show();
+                hideDialog();
+            }
+        }) {
 
-            case R.id.tvForgotPassword:
-                Intent forgotPasswordIntent = new Intent(LoginActivity.this, ForgotPasswordActivity.class);
-                startActivity(forgotPasswordIntent);
-                break;
+            @Override
+            protected Map<String, String> getParams() {
+                // Posting parameters to login url
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("email", email);
+                params.put("password", password);
 
-            case R.id.cbRememberMe:
-                // Content goes here
-                break;
-        }
+                return params;
+            }
+        };
+
+        // Adding request to request queue
+        AppController.getInstance().addToRequestQueue(strReq, tag_string_req);
+    }
+
+    private void showDialog() {
+        if (!pDialog.isShowing())
+            pDialog.show();
+    }
+
+    private void hideDialog() {
+        if (pDialog.isShowing())
+            pDialog.dismiss();
     }
 }
-
