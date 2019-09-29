7dec6a8fa084d1594178a5751b5635e2e1afbe8c
HBedwell24 <bedwellhb@gmail.com>
2019-09-29 14:26:17 -0400

    Overhauled the UI of the login, register, and password reset fragments.

--- app/src/main/java/com/example/navigationdrawer/activity/MainActivity.java
+++ app/src/main/java/com/example/navigationdrawer/activity/MainActivity.java
@@ -3,12 +3,15 @@ package com.example.navigationdrawer.activity;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Rect;
+import android.os.Build;
 import android.support.v4.app.FragmentTransaction;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 
@@ -24,6 +27,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // In Activity's onCreate() for instance
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            Window w = getWindow();
+            w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        }
+
         pref = getSharedPreferences("Constants", Context.MODE_PRIVATE);
         initFragment();
     }
@@ -47,7 +57,7 @@ public class MainActivity extends AppCompatActivity {
 
     private void initFragment() {
 
-        if(pref.getBoolean(Constants.IS_LOGGED_IN,false)) {
+        if (pref.getBoolean(Constants.IS_LOGGED_IN,false)) {
             Intent homeIntent = new Intent(MainActivity.this, NavigationActivity.class);
             startActivity(homeIntent);
         }
--- app/src/main/java/com/example/navigationdrawer/activity/SplashActivity.java
+++ app/src/main/java/com/example/navigationdrawer/activity/SplashActivity.java
@@ -1,35 +1,48 @@
 package com.example.navigationdrawer.activity;
 
 import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.ProgressBar;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
 
 import com.example.navigationdrawer.R;
 
 public class SplashActivity extends AppCompatActivity {
-
-    private ProgressBar progress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        progress = (ProgressBar) findViewById(R.id.progress);
+        RotateAnimation animation = new RotateAnimation(0, 360, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+        animation.setRepeatCount(Animation.INFINITE);
+        animation.setDuration(1000);
+        animation.setInterpolator(new LinearInterpolator());
+
+        // In Activity's onCreate() for instance
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            Window w = getWindow();
+            w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        }
+
+        ImageView imageView = (ImageView) findViewById(R.id.logo);
+        imageView.startAnimation(animation);
 
         new Handler().postDelayed(new Runnable() {
 
             @Override
             public void run() {
-                progress.setVisibility(View.VISIBLE);
                 Intent i = new Intent(SplashActivity.this, MainActivity.class);
                 startActivity(i);
                 finish();
             }
         }, 4000);
-        progress.setVisibility(View.INVISIBLE);
     }
 }
