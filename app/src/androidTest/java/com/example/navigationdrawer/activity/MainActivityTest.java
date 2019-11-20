f4c995faa13d7a4bd1ee7a369ac1a38d0b333003
Hunter Bedwell <32681500+HBedwell24@users.noreply.github.com>
2019-11-20 12:59:23 -0500

    Update MainActivityTest.java

--- app/src/androidTest/java/com/example/navigationdrawer/activity/MainActivityTest.java
+++ app/src/androidTest/java/com/example/navigationdrawer/activity/MainActivityTest.java
@@ -9,6 +9,7 @@ import com.example.navigationdrawer.fragment.RegisterFragment;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.clearText;
 import static androidx.test.espresso.action.ViewActions.click;
@@ -19,45 +20,44 @@ import static androidx.test.espresso.matcher.ViewMatchers.withId;
 public class MainActivityTest {
 
     @Rule
-    public final ActivityTestRule<MainActivity> testRule
-            = new ActivityTestRule<>(MainActivity.class);
+    public final ActivityTestRule<MainActivity> testRule = new ActivityTestRule<>(MainActivity.class);
 
-    /*@Test
+    @Test
     public void signUpTest () {
         LoginFragment loginFragment = new LoginFragment();
         testRule.getActivity().getSupportFragmentManager().beginTransaction()
             .add(R.id.fragment_frame, loginFragment).commit();
         onView(withId(R.id.bRegisterLink)).perform(click());
-    }*/
+    }
 
     @Test
     public void signInTest () {
         RegisterFragment registerFragment = new RegisterFragment();
         testRule.getActivity().getSupportFragmentManager().beginTransaction()
-                .add(R.id.fragment_frame, registerFragment).commit();
+            .add(R.id.fragment_frame, registerFragment).commit();
         onView(withId(R.id.tvLoginLink)).perform(click());
     }
 
-   /* @Test
+    @Test
     public void registerTest () {
         RegisterFragment registerFragment = new RegisterFragment();
         testRule.getActivity().getSupportFragmentManager().beginTransaction()
-                .add(R.id.fragment_frame, registerFragment).commit();
+            .add(R.id.fragment_frame, registerFragment).commit();
 
         onView(withId(R.id.etFullName)).perform(clearText(), typeText("Hunter Bedwell"));
         onView(withId(R.id.etEmail)).perform(clearText(), typeText("bedwellhb@gmail.com"));
         onView(withId(R.id.etPassword)).perform(clearText(), typeText("test123"));
         onView(withId(R.id.bRegister)).perform(click());
-    }*/
+    }
 
-   /* @Test
+    @Test
     public void loginTest() {
         LoginFragment loginFragment = new LoginFragment();
         testRule.getActivity().getSupportFragmentManager().beginTransaction()
-                .add(R.id.fragment_frame, loginFragment).commit();
+            .add(R.id.fragment_frame, loginFragment).commit();
 
         onView(withId(R.id.etEmail)).perform(clearText(), typeText("bedwellhb@gmail.com"));
         onView(withId(R.id.etPassword)).perform(clearText(), typeText("test123"));
         onView(withId(R.id.bLogin)).perform(click());
-    }*/
+    }
 }
